3d0ec738a33c643c40115f1bcd4233ce9790663c
prateek <prateek104104@gmail.com>
2022-11-27 17:23:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let weather = {
+    apikey: "54a176da61806f334b8275c1770a34c4",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".icons").src = "" + icon + ".png";
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+
+};
+document.querySelector(".search button").addEventListener('click', function () {
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener('keyup', function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+weather.fetchWeather("Lucknow");
